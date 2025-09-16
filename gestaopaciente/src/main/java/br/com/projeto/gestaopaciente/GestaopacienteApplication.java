749c1c867dbd46b7e170f989c272359ef6ddf684
Ariadne Cavilha Jorge <ariadne.jorge@teclogica.com.br>
2025-09-16 20:46:55 -0300

    feat: Criação projeto Spring Boot

---
+++ gestaopaciente/src/main/java/br/com/projeto/gestaopaciente/GestaopacienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.gestaopaciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaopacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaopacienteApplication.class, args);
+	}
+
+}
