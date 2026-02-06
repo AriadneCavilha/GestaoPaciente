960a47d66103bb5d50d4c5058c6fcdd76a433ea6
Ariadne Cavilha Jorge <ariadne.jorge@teclogica.com.br>
2026-02-05 21:15:58 -0300

    fix: Ajuste algumas classes e criada a primeira classe paciente

---
+++ gestaopaciente/src/main/java/br/com/projeto/gestaopaciente/service/GestaoPacienteService.java
@@ -0,0 +1,5 @@
+package br.com.projeto.gestaopaciente.service;
+
+public class GestaoPacienteService {
+
+}
---
+++ gestaopaciente/src/main/resources/db/migration/V1__cria_paciente_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE paciente (
+	id BIGINT PRIMARY KEY,
+	name VARCHAR(100),
+	telefone VARCHAR(11),
+	email VARCHAR(120)
+);
